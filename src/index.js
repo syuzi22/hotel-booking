15bc4fe3acfa7e5deda90eca4b464374bd7370b3
Susanna <syuzi22@gmail.com>
2020-06-05 18:50:37 +0300

    Added air-datepicker plugin

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ require.context('', true, /\.(png|svg|jpg)$/);
 import '@item-quantity-dropdown/item-quantity-dropdown.min.js';
 import '@item-quantity-dropdown/item-quantity-dropdown.min.css';
 
+import '../node_modules/air-datepicker/dist/css/datepicker.min.css';
+import '../node_modules/air-datepicker/dist/js/datepicker.min.js';
+
 import './style.scss';
 import './components/guest-dropdown/guest-dropdown.js'
 import './components/room-dropdown/room-dropdown.js'
+import './components/date-dropdown/date-dropdown.js'
+import './components/filterdate-dropdown/filterdate-dropdown.js'
 
-// import './components/colorblock/test.png'
