5c7b463b13189bc6d65f74b6ec97d46dc0dd6dee
Susanna <syuzi22@gmail.com>
2020-06-18 11:56:34 +0300

    Added and styled buttons

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import '@item-quantity-dropdown/item-quantity-dropdown.min.css';
 import '../node_modules/air-datepicker/dist/css/datepicker.min.css';
 import '../node_modules/air-datepicker/dist/js/datepicker.min.js';
 
+import '../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js';
+import '../node_modules/ion-rangeslider/css/ion.rangeSlider.min.css';
+
 import './style.scss';
 import './components/guest-dropdown/guest-dropdown.js'
 import './components/room-dropdown/room-dropdown.js'
@@ -17,5 +20,6 @@ import './components/masked-input/masked-input.js'
 import './components/expand-checkbox/expand-checkbox.js'
 import './components/like-button/like-button.js'
 import './components/rate-button/rate-button.js'
+import './components/range-slider/range-slider.js'
 
 import './pages/form-elements/form-elements.js'
