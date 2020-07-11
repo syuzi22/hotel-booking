a0eae8015aa6e19982b5f56f818af6aa521a5d71
Susanna <syuzi22@gmail.com>
2020-07-11 23:16:23 +0300

    Integrated eslint and jshint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
