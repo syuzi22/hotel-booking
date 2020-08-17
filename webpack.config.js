f9148c6e5708b49ed1b8b86ad0a6295467d85320
Susanna <syuzi22@gmail.com>
2020-08-17 15:31:53 +0300

    Renamed dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,7 @@ module.exports = {
     },
     output: {
         filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "docs"),
     },
     optimization: {
         splitChunks: {
